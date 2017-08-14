6296e4793c8279bee2fce65cb92638e62679029a
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-08-14 17:46:52 -0300

    (01) (08) Boas práticas

---
+++ js/principal.js
@@ -0,0 +1,6 @@
+console.log("carregando arquivo externo");
+	//classe 'titulo'
+	var titulo = document.querySelector(".titulo");
+
+	//muda o texto dentro da tag 'titulo'
+	titulo.textContent = "Rafaela Nutricionista";
