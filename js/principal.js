9cf6fb2ee345ec70dd0cfd7baecd17867c52fb0d
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-08-17 15:52:08 -0300

    (02) Variáveis e operadores (01) Buscndo dados dos pacientes

--- js/principal.js
+++ js/principal.js
@@ -4,3 +4,16 @@ console.log("carregando arquivo externo");
 
 	//muda o texto dentro da tag 'titulo'
 	titulo.textContent = "Rafaela Nutricionista";
+
+	var paciente = document.querySelector("#primeiro_paciente");
+	var tdPeso = paciente.querySelector(".info-peso");
+
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+
+	var altura = tdAltura.textContent;
+
+	var imc = peso / (altura * altura);
+
+	console.log(imc);
