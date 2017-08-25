1e05c0cfda6944b3016e5caa492d014dd1e10118
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-08-25 18:33:50 -0300

    (02) Variáveis e operadores (07) operadores lógicos

--- js/principal.js
+++ js/principal.js
@@ -5,16 +5,40 @@ console.log("carregando arquivo externo");
 	//muda o texto dentro da tag 'titulo'
 	titulo.textContent = "Rafaela Nutricionista";
 
-	var paciente = document.querySelector("#primeiro_paciente");
-	var tdPeso = paciente.querySelector(".info-peso");
+var paciente = document.querySelector("#primeiro_paciente");
+var tdPeso = paciente.querySelector(".info-peso");
 
-	var peso = tdPeso.textContent;
+var peso = tdPeso.textContent;
 
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-	tdIMC = paciente.querySelector(".info-imc");
-	var imc = peso / (altura * altura);
-	tdIMC.textContent = imc;
+tdIMC = paciente.querySelector(".info-imc");
 
-	console.log(imc);
+var pesoEhValido = true;
+var alturaEhvalida = true;
+
+if(peso <= 0 || peso >= 1000){
+		console.log("Peso inválido!");
+		pesoEhValido = false;
+		tdIMC.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+		console.log("Altura inválida!");
+		alturaEhvalida = false;
+		tdIMC.textContent = "Altura inválida!";
+}
+
+
+if (alturaEhvalida && pesoEhValido){
+
+var imc = peso / (altura * altura);
+tdIMC.textContent = imc;
+}
+
+
+
+
+
+console.log(imc);
