7ad4e48f74c431d6974aaba7d9e05980cf0213da
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-08-28 17:18:22 -0300

    (03) Arrays, loop e estilos (01) Replicando a validação e o cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,41 +1,54 @@
-console.log("carregando arquivo externo");
-	//classe 'titulo'
-	var titulo = document.querySelector(".titulo");
+//console.log("carregando arquivo externo");
+//classe 'titulo'
+var titulo = document.querySelector(".titulo");
 
-	//muda o texto dentro da tag 'titulo'
-	titulo.textContent = "Rafaela Nutricionista";
+//muda o texto dentro da tag 'titulo'
+titulo.textContent = "Rafaela Nutricionista";
 
-var paciente = document.querySelector("#primeiro_paciente");
-var tdPeso = paciente.querySelector(".info-peso");
+//var paciente = document.querySelector("#primeiro_paciente");
 
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+	
+	//console.log(pacientes[i]);
+	var paciente = pacientes[i];
+	var tdPeso = paciente.querySelector(".info-peso");
 
-tdIMC = paciente.querySelector(".info-imc");
+	var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhvalida = true;
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
 
-if(peso <= 0 || peso >= 1000){
+	tdIMC = paciente.querySelector(".info-imc");
+
+	var pesoEhValido = true;
+	var alturaEhvalida = true;
+
+	if (peso <= 0 || peso >= 1000) {
 		console.log("Peso inválido!");
 		pesoEhValido = false;
 		tdIMC.textContent = "Peso inválido!";
-}
+	}
 
-if(altura <= 0 || altura >= 3.00){
+	if (altura <= 0 || altura >= 3.00) {
 		console.log("Altura inválida!");
 		alturaEhvalida = false;
 		tdIMC.textContent = "Altura inválida!";
+	}
+
+
+	if (alturaEhvalida && pesoEhValido) {
+
+		var imc = peso / (altura * altura);
+		tdIMC.textContent = imc.toFixed(2);
+	}
 }
 
+console.log(pacientes);
 
-if (alturaEhvalida && pesoEhValido){
 
-var imc = peso / (altura * altura);
-tdIMC.textContent = imc;
-}
+
 
 
 
