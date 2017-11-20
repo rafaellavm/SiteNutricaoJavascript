3cdce79a9c15bccf198690459c20c5f4ae13f165
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-11-20 19:05:14 -0200

    07 - Remoção, delegação e animação - 06- Animando a remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -13,5 +13,11 @@ var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick", function(event) {
     var alvoEvento = event.target;
     var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
-    paiDoAlvo.remove();
+    
+    paiDoAlvo.classList.add("fadeOut");
+    //paiDoAlvo.remove();
+
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
 });
