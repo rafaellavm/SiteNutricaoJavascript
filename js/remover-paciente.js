3d4b4bd5f49ba1bc46761277123bdf91cd0bae69
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-11-20 18:58:02 -0200

    07 - Remoção, delegação e animação - 03 - Delegando eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,17 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function (paciente) {
+/*pacientes.forEach(function (paciente) {
 
     paciente.addEventListener("dblclick", function () {
 
         this.remove();
     });
+});*/
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event) {
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
+    paiDoAlvo.remove();
 });
